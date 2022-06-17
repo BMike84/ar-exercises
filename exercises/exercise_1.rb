0d3ab2bf1fbcf48b5f2316a0fb3612a81ca83ab6
Michael Buffone <m_buffone@hotmail.com>
2022-06-17 07:30:16 -0400

    Deleted store 3 using destroy method and added in instructions to ex1 and 2

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -2,6 +2,13 @@ require_relative '../setup'
 
 puts "Exercise 1"
 puts "----------"
+# 1. Use Active Record's `create` class method multiple times to create 3 stores in the database:
+
+# - Burnaby (annual_revenue of 300000, carries men's and women's apparel)
+# - Richmond (annual_revenue of 1260000 carries women's apparel only)
+# - Gastown (annual_revenue of 190000 carries men's apparel only)
+
+# 2. Output (`puts`) the number of the stores using ActiveRecord's `count` method, to ensure that there are three stores in the database.
 
 # Created Stores
 burnaby = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
